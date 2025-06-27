eae0cdaf4f5c50bf61f440b1d8d5a31613b6c4f4
Giombattista Traina <giombattistatraina354@gmail.com>
2025-06-27 16:49:38 +0200

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,9 @@
+const log = [];
+
+document.addEventListener('keydown', function(event) {
+    log.push(event.key);
+    console.log("Key pressed:", event.key);
+
+    // Aggiorna la textarea
+    document.getElementById('logOutput').value = log.join('');
+});
